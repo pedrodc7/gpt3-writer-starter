cd3ef3119c294456baba50fc3da26a4a7af660d7
pedrodc <pedro.dacunha@gmail.com>
2022-12-01 13:23:31 +0000

    final project commit

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,103 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
+import { useState } from "react";
 
 const Home = () => {
-  return (
-    <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>sup, insert your headline here</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
-          </div>
-        </div>
+   const [userInput, setUserInput] = useState("");
+
+   const [apiOutput, setApiOutput] = useState("");
+   const [isGenerating, setIsGenerating] = useState(false);
+
+   const callGenerateEndpoint = async () => {
+      setIsGenerating(true);
+      console.log("Calling OpenAI");
+      const response = await fetch("/api/generate", {
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json",
+         },
+         body: JSON.stringify({ userInput }),
+      });
+
+      const data = await response.json();
+      const { output } = data;
+      console.log("OpenAI replied...", output.text);
+
+      setApiOutput(`${output.text}`);
+      setIsGenerating(false);
+   };
+
+   const onUserChangedText = (event) => {
+      setUserInput(event.target.value);
+   };
+
+   return (
+      <div className="root">
+         <Head>
+            <title>GPT-3 Writer | buildspace</title>
+         </Head>
+         <div className="container">
+            <div className="header">
+               <div className="header-title">
+                  <h1>Generate the key tasks to deliver a project</h1>
+               </div>
+               <div className="header-subtitle">
+                  <h2>
+                     Describe your project in a short sentence with some context
+                     and you will get the key tasks to accomplish it
+                  </h2>
+               </div>
+            </div>
+            <div className="prompt-container">
+               <textarea
+                  value={userInput}
+                  onChange={onUserChangedText}
+                  placeholder="start typing here"
+                  className="prompt-box"
+               />
+               <div className="prompt-buttons">
+                  <a
+                     className={
+                        isGenerating
+                           ? "generate-button loading"
+                           : "generate-button"
+                     }
+                     onClick={callGenerateEndpoint}
+                  >
+                     <div className="generate">
+                        {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+                     </div>
+                  </a>
+               </div>
+               {apiOutput && (
+                  <div className="output">
+                     <div className="output-header-container">
+                        <div className="output-header">
+                           <h3>Output</h3>
+                        </div>
+                     </div>
+                     <div className="output-content">
+                        <p>{apiOutput}</p>
+                     </div>
+                  </div>
+               )}
+            </div>
+         </div>
+         <div className="badge-container grow">
+            <a
+               href="https://buildspace.so/builds/ai-writer"
+               target="_blank"
+               rel="noreferrer"
+            >
+               <div className="badge">
+                  <Image src={buildspaceLogo} alt="buildspace logo" />
+                  <p>build with buildspace</p>
+               </div>
+            </a>
+         </div>
       </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
-      </div>
-    </div>
-  );
+   );
 };
 
 export default Home;
